6fbdbe9d4ac29ce0d697b6d22b3a89adc4b18c64
Lawrence Abad <laa89@pitt.edu>
2020-09-02 13:32:19 -0400

    Added primer pairing analyses and tighter thermodynamic restrictions

--- src/pde_utils/functions/primers.py
+++ src/pde_utils/functions/primers.py
@@ -1,16 +1,63 @@
-from primer3 import *
+from Bio import SeqUtils
+from primer3 import (
+        calcTm, calcHairpin, calcHomodimer, calcHeterodimer)
 
-def filter_thermostable_oligomers(oligomers, tmMin=52, tmMax=58):
-    thermostable_oligomers = []
+
+def filter_oligomers(oligomers, tmMin=52, tmMax=58, GC_max=60,
+                     hpn_dG_min=-2000, homo_dG_min=-5000):
+    stable_oligomers = []
     for oligomer in oligomers:
-        if calcTm(oligomer) < tmMin or calcTm(oligomer) > tmMax:
-            continue
+        if check_oligomer(oligomer, tmMin=tmMin, tmMax=tmMax, GC_max=GC_max,
+                          hpn_dG_min=hpn_dG_min, homo_dG_min=homo_dG_min):
+            stable_oligomers.append(oligomer)
+
+    return stable_oligomers
 
 
+def check_oligomer(oligomer, tmMin=52, tmMax=58, hpn_dG_min=-2000,
+                   homo_dG_min=-5000, GC_max=60):
+    stable = (check_stable_tm(oligomer, tmMin=tmMin, tmMax=tmMax) and
+              (not check_hairpin(oligomer, delG_min=hpn_dG_min)) and
+              check_optimal_GC(oligomer, GC_max=GC_max) and
+              (not check_homodimer(oligomer, delG_min=homo_dG_min)))
 
-        thermostable_oligomers.append(oligomer)
+    return stable
 
-        
 
-    return thermostable_oligomers
+def check_stable_tm(oligomer, tmMin=52, tmMax=58):
+    tm = calcTm(oligomer)
 
+    return (tm >= tmMin and tm <= tmMax)
+
+
+def check_hairpin(oligomer, delG_min=-2000):
+    hairpin_result = calcHairpin(oligomer)
+
+    if hairpin_result.dg < delG_min:
+        return True
+
+    return False
+
+
+def check_optimal_GC(oligomer, GC_max=60):
+    gc = SeqUtils.GC(oligomer)
+
+    return (gc <= GC_max)
+
+
+def check_homodimer(oligomer, delG_min=-5000):
+    homodimer_result = calcHomodimer(oligomer)
+
+    if homodimer_result.dg < delG_min:
+        return True
+
+    return False
+
+
+def check_heterodimer(oligomer_1, oligomer_2, delG_min=-5000):
+    heterodimer_result = calcHeterodimer(oligomer_1, oligomer_2)
+
+    if heterodimer_result.dg < delG_min:
+        return True
+
+    return False
--- src/pde_utils/pipelines/find_primers.py
+++ src/pde_utils/pipelines/find_primers.py
@@ -6,16 +6,16 @@
 import argparse
 import sys
 import time
+from collections import OrderedDict
 from pathlib import Path
 
+from Bio.Seq import Seq
 from numpy import std
 from pdm_utils.functions import annotation
 from pdm_utils.functions import basic
 from pdm_utils.functions import configfile
-from pdm_utils.functions import mysqldb
 from pdm_utils.functions import pipelines_basic
 from pdm_utils.pipelines import export_db
-from primer3 import calcTm
 
 from pde_utils.functions import primers
 from pde_utils.functions import seq
@@ -28,6 +28,7 @@ PHAGE_QUERY = "SELECT * FROM phage"
 GENE_QUERY = "SELECT * FROM gene"
 TRNA_QUERY = "SELECT * FROM trna"
 TMRNA_QUERY = "SELECT * FROM tmrna"
+
 
 def main(unparsed_args):
     args = parse_find_primers(unparsed_args)
@@ -123,7 +124,10 @@ def parse_find_primers(unparsed_args):
 def execute_find_primers(alchemist, folder_path=None,
                          folder_name=DEFAULT_FOLDER_NAME, values=None,
                          filters="", groups=[], verbose=False,
-                         threads=4, prc=0.8, max_std=500, len_oligomer=20):
+                         threads=4, prc=0.8, max_std=3000, len_oligomer=20,
+                         minD=900, maxD=1100, tmMin=52, tmMax=58,
+                         hpn_dG_min=-500, homo_dG_min=-1000, GC_max=60,
+                         hetero_dG_min=-1000):
     db_filter = pipelines_basic.build_filter(alchemist, "phage", filters)
 
     working_path = pipelines_basic.create_working_path(
@@ -146,96 +150,250 @@ def execute_find_primers(alchemist, folder_path=None,
 
         db_filter.values = db_filter.build_values(where=conditionals)
 
-        parent_genomes = {}
-        for genome_id in db_filter.values:
-            export_db.get_single_genome(alchemist, genome_id,
-                                        data_cache=parent_genomes)
-
-        total_genomes = db_filter.hits()
-        if total_genomes == 0:
+        if db_filter.hits() == 0:
             print("No database entries received from phage "
                   f" for '{mapped_path.name}'.")
 
+        genome_map = {}
+        for genome_id in db_filter.values:
+            export_db.get_single_genome(alchemist, genome_id,
+                                        data_cache=genome_map)
         pham_gene_map = build_pham_gene_map(db_filter, conditionals,
                                             verbose=verbose)
 
-        pham_histogram = {}
-        for pham, genes in pham_gene_map.items():
-            pham_histogram[pham] = len(genes)
-        pham_histogram = basic.sort_histogram(pham_histogram)
+        if verbose:
+            print("Identifying primer pairs for '{mapped_path}'...")
+        primer_pairs = find_primer_pairs(
+                                    alchemist, pham_gene_map, genome_map,
+                                    verbose=verbose, threads=threads, prc=prc,
+                                    max_std=max_std, minD=minD, maxD=maxD,
+                                    len_oligomer=len_oligomer, tmMin=tmMin,
+                                    tmMax=tmMax, hpn_dG_min=hpn_dG_min,
+                                    homo_dG_min=homo_dG_min, GC_max=GC_max)
+
+        primer_pairs = test_primer_pairs(primer_pairs, genome_map,
+                                         verbose=verbose,
+                                         hetero_dG_min=hetero_dG_min)
 
         if verbose:
-            print(f"Identifying prevelant phams for '{mapped_path.name}'...")
+            print(f"Selecting primer_pair from {len(primer_pairs)} "
+                  f"primer pairs for '{mapped_path}'...")
 
-        for pham, count in pham_histogram.items():
-            pham_per = count/total_genomes
-
-            if pham_per < prc:
-                break
-
-            if verbose:
-                print(f"...Analyzing pham {pham}...")
-                print(f"......Pham is represented in {round(pham_per, 3)*100}%"
-                      " of currently viewed genomes...")
-
-            cds_list = export_db.parse_feature_data(alchemist, 
-                                                    values=pham_gene_map[pham])
-
-            starts = []
-            avg_start = 0
-            for cds in cds_list:
-                starts.append(cds.start)
-                avg_start += cds.start
-            avg_start = int(round(avg_start/len(cds_list), 0))
-            start_std = std(starts)
-
-            if start_std > max_std:
-                print(f"......Pham {pham} has unstable synteny...")
-                continue
-
-            cds_to_seq = seq.map_cds_to_seq(alchemist, cds_list, 
-                                                data_cache=parent_genomes)
-            conserved_kmer_data = seq.find_conserved_kmers(cds_to_seq,
-                                                           len_oligomer)
-
-            num_conserved_kmers = len(conserved_kmer_data)
-            if num_conserved_kmers == 0:
-                print(f"......Pham {pham} has no conserved kmers...")
-                continue
-
-            if verbose:
-                print(f"......Pham {pham} has {num_conserved_kmers} "
-                      "conserved kmers...")
-                
-            thermostable_oligomers = primers.filter_thermostable_oligomers(
-                                                    conserved_kmer_data.keys())
-            num_stable_oligomers = len(thermostable_oligomers)
-
-            if num_stable_oligomers == 0:
-                print(f"......Pham {pham} has no stable oligomers...")
-                continue
-            
-            if verbose:
-                print(f"......Pham {pham} has {num_stable_oligomers} "
-                      "stable putative primer oligomers.")  
-
-            for oligomer in thermostable_oligomers:
-                data = conserved_kmer_data[oligomer]
-                avg_oligomer_start = 0
-                for kmer_data in data:
-                    avg_oligomer_start += kmer_data[1]
-                avg_oligomer_start = int(round(avg_oligomer_start/len(data), 
-                                               0))
-
-                avg_oligomer_start = avg_start + avg_oligomer_start
-            
-                print(f".........Oligomer '{oligomer}' is located at "
-                      f"{avg_oligomer_start}")
-        
+        primer_pair = select_primer_pair(primer_pairs)
+        print(primer_pair)
         sys.exit(1)
         pipelines_basic.create_working_dir(working_path)
 
 
+def find_primer_pairs(alchemist, pham_gene_map, genome_map, verbose=False,
+                      threads=4, prc=0.8, max_std=3000, len_oligomer=20,
+                      minD=900, maxD=1100, tmMin=52, tmMax=58,
+                      hpn_dG_min=-500, homo_dG_min=-1000, GC_max=60):
+    pham_histogram = {}
+    for pham, genes in pham_gene_map.items():
+        pham_histogram[pham] = len(genes)
+    pham_histogram = basic.sort_histogram(pham_histogram)
+
+    F_oligomer_pos_map = OrderedDict()
+    R_oligomer_pos_map = OrderedDict()
+    for pham, count in pham_histogram.items():
+        pham_per = count/len(genome_map)
+
+        if pham_per < prc:
+            break
+
+        if verbose:
+            print(f"...Analyzing pham {pham}...")
+            print(f"......Pham is represented in {round(pham_per, 3)*100}%"
+                  " of currently viewed genomes...")
+
+        cds_list = export_db.parse_feature_data(alchemist,
+                                                values=pham_gene_map[pham])
+
+        starts = []
+        avg_start = 0
+        for cds in cds_list:
+            starts.append(cds.start)
+            avg_start += cds.start
+        avg_start = int(round(avg_start/len(cds_list), 0))
+        start_std = std(starts)
+
+        if start_std > max_std:
+            if verbose:
+                print(f"......Pham {pham} has unstable synteny...")
+            continue
+
+        avg_orientation = 0
+        for cds in cds_list:
+            if cds.orientation == "F":
+                avg_orientation += 1
+        avg_orientation = avg_orientation/len(cds_list)
+
+        if avg_orientation != 0 and avg_orientation != 1:
+            if verbose:
+                print(f"......Pham {pham} has unstable direction...")
+            continue
+
+        cds_to_seq = seq.map_cds_to_seq(alchemist, cds_list,
+                                        data_cache=genome_map)
+        conserved_kmer_data = seq.find_conserved_kmers(cds_to_seq,
+                                                       len_oligomer)
+
+        num_conserved_kmers = len(conserved_kmer_data)
+        if num_conserved_kmers == 0:
+            if verbose:
+                print(f"......Pham {pham} has no conserved kmers...")
+            continue
+
+        if verbose:
+            print(f"......Pham {pham} has {num_conserved_kmers} "
+                  "conserved kmers...")
+
+        F_oligomers_cds_map = get_oriented_oligomers(
+                                        conserved_kmer_data, "F")
+        R_oligomers_cds_map = get_oriented_oligomers(
+                                        conserved_kmer_data, "R")
+
+        F_oligomers = primers.filter_oligomers(
+                                    F_oligomers_cds_map.keys(), tmMin=tmMin,
+                                    tmMax=tmMax, hpn_dG_min=hpn_dG_min,
+                                    homo_dG_min=homo_dG_min, GC_max=GC_max)
+        R_oligomers = primers.filter_oligomers(
+                                    R_oligomers_cds_map.keys(), tmMin=tmMin,
+                                    tmMax=tmMax, hpn_dG_min=hpn_dG_min,
+                                    homo_dG_min=homo_dG_min, GC_max=GC_max)
+        num_stable_oligomers = len(F_oligomers) + len(R_oligomers)
+
+        if num_stable_oligomers == 0:
+            if verbose:
+                print(f"......Pham {pham} has no stable oligomers...")
+            continue
+
+        if verbose:
+            print(f"......Pham {pham} has {len(F_oligomers)} "
+                  "stable forward putative primer oligomers.")
+            print(f"......Pham {pham} has {len(R_oligomers)} "
+                  "stable reverse putative primer oligomers.")
+
+        pham_F_oligomer_pos_map = map_oligomer_to_pos(F_oligomers_cds_map,
+                                                      F_oligomers,
+                                                      avg_start,
+                                                      verbose=verbose)
+
+        pham_R_oligomer_pos_map = map_oligomer_to_pos(R_oligomers_cds_map,
+                                                      R_oligomers,
+                                                      avg_start,
+                                                      verbose=verbose)
+
+        F_oligomer_pos_map.update(pham_F_oligomer_pos_map)
+        R_oligomer_pos_map.update(pham_R_oligomer_pos_map)
+
+    primer_pairs = []
+
+    F_pos_oligomer_map = invert_dictionary(F_oligomer_pos_map)
+    R_pos_oligomer_map = invert_dictionary(R_oligomer_pos_map)
+
+    for pos, F_oligomers in F_pos_oligomer_map.items():
+        for i in range(minD, maxD+1):
+            R_oligomers = R_pos_oligomer_map.get(pos+i)
+            if R_oligomers:
+                for F_oligomer in F_oligomers:
+                    for R_oligomer in R_oligomers:
+                        pair_dict = {}
+                        pair_dict["forward"] = F_oligomer
+                        pair_dict["reverse"] = R_oligomer
+                        pair_dict["length"] = i
+                        pair_dict["start"] = pos
+                        primer_pairs.append(pair_dict)
+
+    primer_pairs = sorted(primer_pairs, key=lambda pair: pair['start'])
+    return primer_pairs
+
+
+def test_primer_pairs(primer_pairs, genomes, verbose=False,
+                      hetero_dG_min=-1000):
+    tested_primer_pairs = []
+
+    num_pairs = len(primer_pairs)
+    for i in range(num_pairs):
+        primer_pair = primer_pairs[i]
+        if verbose:
+            print(f"...Testing primer pair {i} out of "
+                  f"{num_pairs} putative primers...")
+
+        if primers.check_heterodimer(
+                        primer_pair["forward"], primer_pair["reverse"],
+                        delG_min=hetero_dG_min):
+            continue
+
+        tested_primer_pairs.append(primer_pair)
+
+    return tested_primer_pairs
+
+
+def select_primer_pair(primer_pairs):
+    return primer_pairs[0]
+
+
+# TO FIX IN BASIC
+def invert_dictionary(dictionary):
+    """Inverts a dictionary, where the values and keys are swapped.
+
+    :param dictionary: A dictionary to be inverted.
+    :type dictionary: dict
+    :returns: Returns an inverted dictionary of the given dictionary.
+    :rtype: dict
+    """
+    new_dict = {}
+    for key, value in dictionary.items():
+        if isinstance(value, list):
+            for subvalue in value:
+                if new_dict.get(subvalue) is None:
+                    new_dict[subvalue] = [key]
+                else:
+                    new_dict[subvalue].append(key)
+        else:
+            if new_dict.get(value) is None:
+                new_dict[value] = [key]
+            else:
+                new_dict[value].append(key)
+
+    return new_dict
+
+
+def get_oriented_oligomers(conserved_kmer_data, orientation):
+    oligomers_cds_map = {}
+
+    for kmer, kmer_data in conserved_kmer_data.items():
+        if kmer_data[0][0].orientation != orientation:
+            kmer = str(Seq(kmer).reverse_complement())
+
+        oligomers_cds_map[kmer] = kmer_data
+
+    return oligomers_cds_map
+
+
+def map_oligomer_to_pos(oligomers_cds_map, oligomers, avg_start,
+                        verbose=False):
+    oligomer_pos_map = OrderedDict()
+    for oligomer in oligomers:
+        data = oligomers_cds_map[oligomer]
+        avg_oligomer_start = 0
+        for kmer_data in data:
+            avg_oligomer_start += kmer_data[1]
+        avg_oligomer_start = int(round(avg_oligomer_start/len(data),
+                                       0))
+
+        avg_oligomer_start = avg_start + avg_oligomer_start
+
+        oligomer_pos_map[oligomer] = avg_oligomer_start
+
+    oligomer_pos_map = basic.sort_histogram(oligomer_pos_map)
+
+    return oligomer_pos_map
+
+
+# TO RELOCATE
 def build_pham_gene_map(db_filter, conditionals, verbose=False):
     gene_col = db_filter.get_column("gene.GeneID")
     pham_col = db_filter.get_column("gene.PhamID")
@@ -251,6 +409,7 @@ def build_pham_gene_map(db_filter, conditionals, verbose=False):
     return pham_gene_map
 
 
+# TO FIX IN ANNOTATION
 def get_count_phams_in_genes(alchemist, geneids, incounts=None):
     phams = annotation.get_phams_from_genes(alchemist, geneids)
 
