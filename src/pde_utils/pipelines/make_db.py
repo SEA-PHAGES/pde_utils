19f6e0fe19b508a721d869cfdd39e7fa369f5482
laa89 <laa89@pitt.edu>
2020-09-13 15:31:12 -0400

    Cleaned make_db parser

--- src/pde_utils/pipelines/make_db.py
+++ src/pde_utils/pipelines/make_db.py
@@ -2,44 +2,41 @@ import argparse
 import os
 import shutil
 import shlex
-import subprocess
 import sys
-import textwrap
 import time
 from pathlib import Path
 from subprocess import DEVNULL, PIPE, Popen
 
-from Bio import AlignIO
-from pdm_utils.functions import basic
-from pdm_utils.functions import fileio
 from pdm_utils.functions import pipelines_basic
 
-from pde_utils.classes import clustal
 from pde_utils.functions import alignment
 
-#GLOBAL VARIABLES
-#-----------------------------------------------------------------------------
+# GLOBAL VARIABLES
+# -----------------------------------------------------------------------------
 
-DATABASE_TYPES = ["hhsuite", "mmseqs"]
+DATABASE_TYPES = ["hhsuite", "mmseqs", "blast"]
 
 DEFAULT_FOLDER_NAME = f"{time.strftime('%Y%m%d')}_db"
 DEFAULT_FOLDER_PATH = Path.cwd()
 
 PHAM_FASTA_COLUMNS = ["gene.GeneID", "gene.Translation"]
 
-#MAIN FUNCTIONS
-#-----------------------------------------------------------------------------
+# MAIN FUNCTIONS
+# -----------------------------------------------------------------------------
+
+
 def main(unparsed_args_list):
     args = parse_make_db(unparsed_args_list)
 
-    alchemist = pipelines_basic.build_alchemist(args.database) 
+    alchemist = pipelines_basic.build_alchemist(args.database)
     values = pipelines_basic.parse_value_input(args.input)
 
     execute_make_db(alchemist, args.folder_path, args.folder_name,
                     args.db_type, values=values, verbose=args.verbose,
-                    filters=args.filters, groups=args.groups, 
-                    db_name=args.db_name, threads=args.number_threads)
-    
+                    filters=args.filters, groups=args.groups,
+                    db_name=args.db_name, threads=args.threads)
+
+
 def parse_make_db(unparsed_args_list):
     DATABASE_HELP = """
         Name of the MySQL database to export from.
@@ -99,7 +96,7 @@ def parse_make_db(unparsed_args_list):
     parser.add_argument("db_type", type=str, choices=DATABASE_TYPES,
                         help=DATABASE_TYPE_HELP)
 
-    parser.add_argument("-m", "--folder_name", type=str, 
+    parser.add_argument("-m", "--folder_name", type=str,
                         help=FOLDER_NAME_HELP)
     parser.add_argument("-o", "--folder_path",
                         type=pipelines_basic.convert_dir_path,
@@ -109,7 +106,7 @@ def parse_make_db(unparsed_args_list):
 
     parser.add_argument("-dbm", "--database_name", type=str,
                         help=DATABASE_NAME_HELP)
-    parser.add_argument("-np", "--number_threads", type=int, nargs="?",
+    parser.add_argument("-np", "--threads", type=int, nargs="?",
                         help=NUMBER_THREADS_HELP)
 
     parser.add_argument("-if", "--import_file",
@@ -117,41 +114,41 @@ def parse_make_db(unparsed_args_list):
                         help=IMPORT_FILE_HELP, dest="input")
     parser.add_argument("-in", "--import_names", nargs="*",
                         help=IMPORT_NAMES_HELP, dest="input")
-    parser.add_argument("-f", "--where", nargs="?",
+    parser.add_argument("-w", "--where", nargs="?",
                         help=WHERE_HELP,
                         dest="filters")
     parser.add_argument("-g", "--group_by", nargs="+",
                         help=GROUP_BY_HELP,
-                        dest="groups") 
+                        dest="groups")
 
     parser.set_defaults(folder_name=DEFAULT_FOLDER_NAME,
                         folder_path=DEFAULT_FOLDER_PATH,
                         verbose=False, input=[],
                         filters="", groups=[],
-                        db_name=None, number_threads=1) 
+                        db_name=None, threads=1)
 
-    parsed_args = parser.parse_args(unparsed_args_list[1:])
+    parsed_args = parser.parse_args(unparsed_args_list[2:])
     return parsed_args
-    
+
+
 def execute_make_db(alchemist, folder_path, folder_name, db_type,
                     values=None, verbose=False, filters="", groups=[],
                     db_name=None, threads=1):
     if db_name is None:
         db_name = alchemist.database
-    
+
     db_filter = pipelines_basic.build_filter(alchemist, "pham", filters,
                                              values=values,
                                              verbose=verbose)
 
-    db_path = folder_path.joinpath(folder_name)
-    db_path = basic.make_new_dir(folder_path, db_path, attempt=50)
+    working_path = pipelines_basic.create_working_path(folder_path,
+                                                       folder_name)
+
+    conditionals_map = pipelines_basic.build_groups_map(
+                                            db_filter, working_path,
+                                            groups=groups, verbose=verbose)
 
     data_cache = {}
-    conditionals_map = {}
-    pipelines_basic.build_groups_map(db_filter, db_path,
-                                     conditionals_map,
-                                     groups=groups, verbose=verbose)
-
     values = db_filter.values
     for mapped_path in conditionals_map.keys():
         db_filter.reset()
@@ -163,26 +160,26 @@ def execute_make_db(alchemist, folder_path, folder_name, db_type,
         if db_filter.hits() == 0:
             print(f"No database entries received for '{mapped_path}'.")
             continue
-  
+
+        pipelines_basic.create_working_dir(mapped_path)
+
         if db_type == "hhsuite":
             execute_make_hhsuite_database(alchemist, db_filter.values,
-                                          db_path, db_name,
+                                          mapped_path, db_name,
                                           data_cache=data_cache,
                                           threads=threads)
 
-def execute_make_hhsuite_database(alchemist, values, db_dir, db_name, 
-                                                              data_cache=None,
-                                                              verbose=False,
-                                                              threads=1):
+
+def execute_make_hhsuite_database(alchemist, values, db_dir, db_name,
+                                  data_cache=None, verbose=False, threads=1):
     aln_dir = db_dir.joinpath("pham_alignments")
     aln_dir.mkdir()
 
-    fasta_path_map = alignment.create_pham_fastas(alchemist.engine, 
-                                                  values, aln_dir, 
-                                                  data_cache=data_cache,
-                                                  verbose=verbose)
-    alignment.align_pham_fastas(fasta_path_map, threads=threads, 
-                                                  verbose=verbose)
+    fasta_path_map = alignment.create_pham_fasta(
+                                    alchemist.engine, values, aln_dir,
+                                    data_cache=data_cache, verbose=verbose)
+    alignment.align_pham_fastas(fasta_path_map, threads=threads,
+                                verbose=verbose)
 
     if verbose:
         stdout = PIPE
@@ -191,20 +188,15 @@ def execute_make_hhsuite_database(alchemist, values, db_dir, db_name,
 
     msa_fftuple = create_msa_ffindex(aln_dir, db_dir, db_name, stdout=stdout)
     a3m_fftuple = create_a3m_ffindex(db_dir, db_name, msa_fftuple,
-                                                  threads=threads, 
-                                                  stdout=stdout)
-    hmm_fftuple = create_hmm_ffindex(db_dir, db_name, a3m_fftuple, 
-                                                  threads=threads,
-                                                  stdout=stdout)
+                                     threads=threads, stdout=stdout)
+    hmm_fftuple = create_hmm_ffindex(db_dir, db_name, a3m_fftuple,
+                                     threads=threads, stdout=stdout)
     create_cs219_ffindex(db_dir, db_name, stdout=stdout)
 
     sorting_file = create_sorting_file(db_dir, db_name, stdout=stdout)
-    sort_a3m_ffindex(db_dir, db_name, sorting_file, a3m_fftuple, 
-                                                  stdout=stdout)
-    sort_hmm_ffindex(db_dir, db_name, sorting_file, hmm_fftuple, 
-                                                  stdout=stdout)
+    sort_a3m_ffindex(db_dir, db_name, sorting_file, a3m_fftuple, stdout=stdout)
+    sort_hmm_ffindex(db_dir, db_name, sorting_file, hmm_fftuple, stdout=stdout)
 
-    
     msa_fftuple[0].unlink()
     msa_fftuple[1].unlink()
     sorting_file.unlink()
@@ -214,15 +206,16 @@ def execute_make_hhsuite_database(alchemist, values, db_dir, db_name,
               f"at '{db_dir}'.\n  Scrapping database.")
         shutil.rmtree(db_dir)
 
-#HHSUITE DB HELPER FUNCTIONS
-#-----------------------------------------------------------------------------
+
+# HHSUITE DB HELPER FUNCTIONS
+# -----------------------------------------------------------------------------
 def create_msa_ffindex(fasta_dir, db_dir, db_name, stdout=None):
     if stdout is None:
         stdout = DEVNULL
-    
+
     msa_ffdata = db_dir.joinpath("".join([db_name, "_msa.ffdata"]))
     msa_ffindex = db_dir.joinpath("".join([db_name, "_msa.ffindex"]))
-    
+
     command = f"ffindex_build -s {msa_ffdata} {msa_ffindex} {fasta_dir}"
 
     split_command = shlex.split(command)
@@ -231,18 +224,20 @@ def create_msa_ffindex(fasta_dir, db_dir, db_name, stdout=None):
 
     return (msa_ffdata, msa_ffindex)
 
+
 def create_a3m_ffindex(db_dir, db_name, msa_fftuple, threads=1, stdout=None):
     if stdout is None:
         stdout = DEVNULL
 
     a3m_ffdata = db_dir.joinpath("".join([db_name, "_a3m.ffdata"]))
     a3m_ffindex = db_dir.joinpath("".join([db_name, "_a3m.ffindex"]))
-  
+
     os.environ["OMP_NUM_THREADS"] = "1"
     command = (f"mpirun -np {threads} "
                f"ffindex_apply_mpi {msa_fftuple[0]} {msa_fftuple[1]} "
                f"-i {a3m_ffindex} -d {a3m_ffdata} "
-                "-- hhconsensus -M 50 -maxres 65535 -i stdin -oa3m stdout -v 0")
+               "-- hhconsensus -M 50 -maxres 65535 "
+               "-i stdin -oa3m stdout -v 0")
 
     split_command = shlex.split(command)
     with Popen(args=split_command, stdout=stdout, stderr=stdout) as process:
@@ -250,23 +245,25 @@ def create_a3m_ffindex(db_dir, db_name, msa_fftuple, threads=1, stdout=None):
 
     return (a3m_ffdata, a3m_ffindex)
 
+
 def create_hmm_ffindex(db_dir, db_name, a3m_fftuple, threads=1, stdout=None):
     if stdout is None:
         stdout = DEVNULL
 
     hmm_ffdata = db_dir.joinpath("".join([db_name, "_hmm.ffdata"]))
     hmm_ffindex = db_dir.joinpath("".join([db_name, "_hmm.ffindex"]))
-    
+
     command = (f"mpirun -np {threads} ffindex_apply_mpi "
-               f"{a3m_fftuple[0]} {a3m_fftuple[1]} " 
+               f"{a3m_fftuple[0]} {a3m_fftuple[1]} "
                f"-i {hmm_ffindex} -d {hmm_ffdata} "
-                "-- hhmake -i stdin -o stdout -v 0")
+               "-- hhmake -i stdin -o stdout -v 0")
 
     split_command = shlex.split(command)
     with Popen(args=split_command, stdout=stdout, stderr=stdout) as process:
         out, errors = process.communicate()
-    
+
     return (hmm_ffdata, hmm_ffindex)
+
 
 def create_cs219_ffindex(db_dir, db_name, stdout=None):
     if stdout is None:
@@ -274,47 +271,50 @@ def create_cs219_ffindex(db_dir, db_name, stdout=None):
 
     a3m_ffpath = db_dir.joinpath("".join([db_name, "_a3m"]))
     cs219_ffpath = db_dir.joinpath("".join([db_name, "_cs219"]))
-    
+
     command = ("cstranslate -f -x 0.3 -c 4 -I a3m "
-              f"-i {a3m_ffpath} -o {cs219_ffpath}")
+               f"-i {a3m_ffpath} -o {cs219_ffpath}")
 
     split_command = shlex.split(command)
     with Popen(args=split_command, stdout=stdout, stderr=stdout) as process:
         out, errors = process.communicate()
+
 
 def create_sorting_file(db_dir, db_name, sorting_file=None, stdout=None):
     if sorting_file is None:
         sorting_file = db_dir.joinpath("sorting.dat")
 
     cs219_ffindex = db_dir.joinpath("".join([db_name, "_cs219.ffindex"]))
-    
+
     sort_command = f"sort -k3 -n -r {cs219_ffindex} "
     cut_command = "cut -f1"
 
     split_sort_command = shlex.split(sort_command)
     split_cut_command = shlex.split(cut_command)
-    
+
     sort_process = Popen(args=split_sort_command, stdout=PIPE)
     cut_process = Popen(args=split_cut_command, stdout=PIPE,
-                                                  stdin=sort_process.stdout)
+                        stdin=sort_process.stdout)
     sort_process.stdout.close()
 
     file_handle = sorting_file.open(mode="w")
     file_handle.write((cut_process.communicate()[0]).decode("utf-8"))
     file_handle.close()
-    
+
     return sorting_file
+
 
 def sort_a3m_ffindex(db_dir, db_name, sorting_file, a3m_tuple, stdout=None):
     if stdout is None:
         stdout = DEVNULL
 
     ord_a3m_ffdata = db_dir.joinpath("".join([db_name, "ordered_a3m.ffdata"]))
-    ord_a3m_ffindex = db_dir.joinpath("".join([db_name, "ordered_a3m.ffindex"]))
+    ord_a3m_ffindex = db_dir.joinpath("".join(
+                                            [db_name, "ordered_a3m.ffindex"]))
 
     command = (f"ffindex_order {sorting_file} {a3m_tuple[0]} {a3m_tuple[1]} "
                f"{ord_a3m_ffdata} {ord_a3m_ffindex}")
-    
+
     split_command = shlex.split(command)
     with Popen(args=split_command, stdout=stdout, stderr=stdout) as process:
         out, errors = process.communicate()
@@ -322,16 +322,18 @@ def sort_a3m_ffindex(db_dir, db_name, sorting_file, a3m_tuple, stdout=None):
     ord_a3m_ffdata.replace(a3m_tuple[0])
     ord_a3m_ffindex.replace(a3m_tuple[1])
 
+
 def sort_hmm_ffindex(db_dir, db_name, sorting_file, hmm_tuple, stdout=None):
     if stdout is None:
         stdout = DEVNULL
 
     ord_hmm_ffdata = db_dir.joinpath("".join([db_name, "ordered_hmm.ffdata"]))
-    ord_hmm_ffindex = db_dir.joinpath("".join([db_name, "ordered_hmm.ffindex"]))
+    ord_hmm_ffindex = db_dir.joinpath("".join(
+                                            [db_name, "ordered_hmm.ffindex"]))
 
     command = (f"ffindex_order {sorting_file} {hmm_tuple[0]} {hmm_tuple[1]} "
                f"{ord_hmm_ffdata} {ord_hmm_ffindex}")
-    
+
     split_command = shlex.split(command)
     with Popen(args=split_command, stdout=stdout, stderr=stdout) as process:
         out, errors = process.communicate()
@@ -339,11 +341,13 @@ def sort_hmm_ffindex(db_dir, db_name, sorting_file, hmm_tuple, stdout=None):
     ord_hmm_ffdata.replace(hmm_tuple[0])
     ord_hmm_ffindex.replace(hmm_tuple[1])
 
+
 def verify_hhsuite_database(db_dir, db_name, threads=1, stdout=None):
-    if stdout == None:
+    if stdout is None:
         stdout = DEVNULL
 
     return True
+
 
 if __name__ == "__main__":
     main(sys.argv)
